3c1f71c0969415f53cf734c41b2d4971ee426877
B6230760 <84361704+B6230760@users.noreply.github.com>
2021-11-04 09:41:35 +0700

    Update preorder.go

--- backend/controller/preorder.go
+++ backend/controller/preorder.go
@@ -8,7 +8,7 @@ import (
 )
 
 // POST /preorderlists
-func CreatePreorderList(c *gin.Context) {
+func CreatePreorder(c *gin.Context) {
 	var preorder entity.Preorder
 	if err := c.ShouldBindJSON(&preorder); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -22,8 +22,8 @@ func CreatePreorderList(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": preorder})
 }
 
-// GET /preorderlist/:id
-func GetPreorderList(c *gin.Context) {
+// GET /preorder/:id
+func GetPreorder(c *gin.Context) {
 	var preorder entity.Preorder
 	id := c.Param("id")
 	if err := entity.DB().Raw("SELECT * FROM preorders WHERE id = ?", id).Scan(&preorder).Error; err != nil {
@@ -34,8 +34,8 @@ func GetPreorderList(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": preorder})
 }
 
-// GET /preorderlists
-func ListPreorderLists(c *gin.Context) {
+// GET /preorders
+func ListPreorders(c *gin.Context) {
 	var preorders []entity.Preorder
 	id := c.Param("id")
 	if err := entity.DB().Raw("SELECT * FROM preorders WHERE user_id = ?", id).Scan(&preorders).Error; err != nil {
@@ -46,8 +46,8 @@ func ListPreorderLists(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": preorders})
 }
 
-// DELETE /preorderlists/:id
-func DeletePreorderList(c *gin.Context) {
+// DELETE /preorders/:id
+func DeletePreorder(c *gin.Context) {
 	id := c.Param("id")
 	if tx := entity.DB().Exec("DELETE FROM preorders WHERE id = ?", id); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "preorder not found"})
@@ -57,8 +57,8 @@ func DeletePreorderList(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": id})
 }
 
-// PATCH /preorderlists
-func UpdatePreorderList(c *gin.Context) {
+// PATCH /preorders
+func UpdatePreorder(c *gin.Context) {
 	var preorder entity.Preorder
 	if err := c.ShouldBindJSON(&preorder); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
